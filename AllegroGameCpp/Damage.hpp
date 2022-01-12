7698045e9c75c8217aac3bf1805c4e3a8e5940d4
Luis Eduardo Pereira Mendes <dudupemendes13@gmail.com>
2022-01-12 13:01:03 -0300

    refactor

--- AllegroGameCpp/Damage.hpp
+++ AllegroGameCpp/Damage.hpp
@@ -2,13 +2,18 @@
 
 class DmgAndColision{
 public:
+    /*damage*/
     bool projectileHitPlayer(Projectile *projectile, Player *player);
     bool enemyHitPlayer(Enemies* enemy, Player* player);
     bool playerProjectileHit(Enemies* enemy, Player *player);
-    bool isNextPositionPlayerValidEach(Player player, Obstacles obst);
-    bool isNextPositionEnemyValidEach(Enemies enemy, Obstacles obst);
-    bool isNextPositionProjectileValid(Projectile projectile, Obstacles obst);
+
+    /*colision*/
     bool isNextPositionProjValid(Projectile proj, Obstacles obs[]);
     bool isNextPositionPlayerValid(Player player, Obstacles obs[]);
     bool isNextPositionEnemyValid(Enemies enemy, Obstacles obs[]);
+
+private:
+    bool isNextPositionPlayerValidEach(Player player, Obstacles obst);
+    bool isNextPositionEnemyValidEach(Enemies enemy, Obstacles obst);
+    bool isNextPositionProjectileValid(Projectile projectile, Obstacles obst);
 };
